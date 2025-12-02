e95bd7298d4ad3e77eba256b39025ce1a1e53bf7
CFTaborda <tabordacristianfernando@gmail.com>
2025-12-02 10:28:57 -0300

    useWallet

--- src/hooks/web3/useWallet.ts
+++ src/hooks/web3/useWallet.ts
@@ -1,6 +1,21 @@
 import { useAccount, useChainId, useDisconnect, useSwitchChain } from 'wagmi';
 import { open } from '@reown/appkit/react';
-import { arbitrumSepolia, sepolia } from '@reown/appkit/networks';
+
+const arbitrumSepolia = {
+  id: 421614,
+  name: 'Arbitrum Sepolia',
+  network: 'arbitrum-sepolia',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: { default: { http: ['https://sepolia-rollup.arbitrum.io/rpc'] } },
+} as const;
+
+const sepolia = {
+  id: 11155111,
+  name: 'Sepolia',
+  network: 'sepolia',
+  nativeCurrency: { name: 'Sepolia Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: { default: { http: ['https://ethereum-sepolia.blockpi.network/v1/rpc/public'] } },
+} as const;
 
 const SUPPORTED_CHAINS = [arbitrumSepolia, sepolia] as const;
 
@@ -9,9 +24,10 @@ export function useWallet() {
   const chainId = useChainId();
   const { disconnectAsync } = useDisconnect();
   const { switchChainAsync, isPending: isSwitching } = useSwitchChain();
-  const openModal = () => open();
-  const openNetworks = () => open({ view: 'Networks' });
+
+  const connect = () => open();
   const openAccount = () => open({ view: 'Account' });
+  const openNetworks = () => open({ view: 'Networks' });
 
   const switchToArbitrum = async () => {
     if (chainId !== arbitrumSepolia.id) {
@@ -34,17 +50,16 @@ export function useWallet() {
     isConnected,
     chainId,
     connector,
+    currentChain: SUPPORTED_CHAINS.find(c => c.id === chainId),
 
-    connect: openModal,
+    connect,
     disconnect,
-    openModal,
     openAccount,
     openNetworks,
     switchToArbitrum,
     switchToSepolia,
     isSwitching,
 
-    currentChain: SUPPORTED_CHAINS.find(c => c.id === chainId),
     supportedChains: SUPPORTED_CHAINS,
   };
 }
