592162086eb0cc7e0b5ec033c4df07c6b94a31b5
cristianftab <ethernitydao@gmail.com>
2025-12-16 14:03:19 -0300

    useRequireCorrectNetwork

--- src/hooks/web3/index.ts
+++ src/hooks/web3/index.ts
@@ -4,3 +4,4 @@ export { useNetwork } from './useNetwork';
 export { useCorrectChain } from './useCorrectChain';
 export { useTokenBalance } from './useTokenBalance';
 export { useWallet } from './useWallet';
+export { useRequireCorrectNetwork } from './useRequireCorrectNetwork';
